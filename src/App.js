3c13f6ff099e61ce6e24757b1d6c81af037ae4e8
tianzhen <tianzhen0119@gmail.com>
2023-12-13 09:55:31 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useLayoutEffect, useState } from "react";
+
+const initialList = [
+  {text:"apple",
+   isDone:false,
+  },
+  {text:"orange",
+  isDone:false,
+ },
+ {text:"banana",
+ isDone:false,
+}
+]
+
 
 function App() {
+const [shoppingList,setShoppingList] =useState(initialList)
+
+function handleAdd(addItem){
+  setShoppingList(shoppingList=>[...shoppingList,addItem])
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <AddShoppingListForm onAddList={handleAdd}/>
+      <ShoppingList shoppingList={shoppingList}/>
     </div>
   );
 }
+
+function AddShoppingListForm({onAddList}){
+ const [newItem,setNewItem] = useState("")
+
+function handleAddShoppingList(e){
+  e.preventDefault()
+
+  if(!newItem) return 
+  const addItem = {
+    text:newItem,
+    isDone:false,
+  }
+
+  onAddList(addItem)
+
+  setNewItem("")
+}
+
+  return <form onSubmit={handleAddShoppingList} className="add-form">
+    <label>Add your list</label>
+    <input 
+    type="text"
+    value={newItem} 
+    onChange={(e)=>setNewItem(e.target.value)}
+    />
+    <button>ADD</button>
+  </form>
+}
+
+function ShoppingList({shoppingList}){
+  return <ul className="shopping-list">
+     {
+    shoppingList.map((item,index)=>(<ShoppingItem shoppingItem={item} key={index}  />))
+  }
+  </ul>
+}
+
+function ShoppingItem({shoppingItem}){
+  const [isChecked,setIsChecked] = useState(false)
+
+
+  return  (
+    <li 
+    className={isChecked ? "shopping-item checked" :"shopping-item"}
+    >
+      <input 
+      type="checkbox"
+      onChange={e=>setIsChecked(e.target.checked)}
+      />
+      <span>{shoppingItem.text}</span>
+    </li>
+  )
+  
+}
+
 
 export default App;
